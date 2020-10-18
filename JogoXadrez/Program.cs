9892ea590eedb829472d8212cb9cc4f23756545a
Lizandra <lizandra.oliveirasouza@hotmail.com>
2020-10-18 16:33:03 -0300

    inicializacao projeto

---
+++ JogoXadrez/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JogoXadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
